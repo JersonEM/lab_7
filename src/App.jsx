63ad1adc2bca5f0b3a7556ee5914e40a4addc202
Jerson Elias <128757339+JersonEM@users.noreply.github.com>
2024-04-14 17:55:18 -0400

    3rd commit
    randomyze the number for generate a number from 1 to 100

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css'
-import { List } from './components/List';
-import { WapperList } from './components/WrapperList';
+import { Button } from './components/Button'
 
 function App() {
 
+  const buttonText = "Count : ";
+
   return (
     <>
-      <WapperList>
-        <List/>
-      </WapperList>
+      <Button text={buttonText}/>
     </>
   )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,16 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-export const Button = props => {
+export const Button = ({ text }) => {
+    const [randomNumber, setRandomNumber] = useState(null);
 
-    const { text } = props;
-
-    const[count, setCount] = useState(0);
-
-    function handlerButton(){
-        setCount(count + 1);
-    }
+    const generateRandomNumber = () => {
+        const newRandomNumber = Math.floor(Math.random() * 100) + 1;
+        setRandomNumber(newRandomNumber);
+    };
 
     return (
-        <button onClick={ handlerButton }>
-            { text } { count }
+        <button onClick={generateRandomNumber}>
+            {text} {randomNumber !== null ? randomNumber : "Generar un numero aleatorio"}
         </button>
     );
-}
+};
