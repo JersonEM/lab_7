da89c6522e791d87ae36f8843096cbc45075ab2b
Jerson Elias <128757339+JersonEM@users.noreply.github.com>
2024-04-14 16:35:32 -0400

    1st commit
    1st project upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { List } from './components/List';
+import { WapperList } from './components/WrapperList';
+
+function App() {
+
+  return (
+    <>
+      <WapperList>
+        <List/>
+      </WapperList>
+    </>
+  )
+}
+
+export default App
